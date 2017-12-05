d6e53f6777b2414d63398133f277cbdb41787733
Miguel Sanchez <msanchez.anaheim@gmail.com>
2017-12-04 20:33:23 -0800

    Added authentication to the application (Google)

--- index2.js
+++ index2.js
@@ -2,37 +2,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-// const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/user');
 require('./services/passport');
 
-// connect to mongoDB using mongoose
-// mongoose.Promise = global.Promise;
-mongoose.connect(keys.mongoURI);
-
 const app = express();
-
-// passport config
-app.use(
-    cookieSession({
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        keys: [keys.cookieKey]
-    })
-);
-app.use(passport.initialize());
-app.use(passport.session());
 
 // homepage route
 app.get('/', (req, res) => {
     res.send('Hello, world');
 })
 
-// routes
-require('./routes/authRoutes')(app);
-
 // start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 9090;
 app.listen(PORT, () => {
     console.log('Server is online');
 })
