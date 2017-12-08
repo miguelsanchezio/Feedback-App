d33d56784fd66054ebd745cdd2f47b876c1dfe9d
Miguel Sanchez <msanchez.anaheim@gmail.com>
2017-12-08 11:43:35 -0800

    Styled header, created and added the Landing.js component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link }  from 'react-router-dom';
 
 class Header extends Component {
+    renderContent() {
+        switch(this.props.auth) {
+            case null:
+                return;
+            case false:
+                return (
+                    <li><a href='/auth/google'>Login With Google</a></li>
+                )
+            default:
+                return <li><a href='/api/logout'>Logout</a></li>
+        }
+    }
+
     render() {
         return (
             <nav>
                 <div className='nav-wrapper'>
-                    <a href="/" className='brand-logo left'>Feedback App</a>
+                    <Link to={this.props.auth ? '/surveys' : '/'}
+                    className='left brand-logo'
+                    >
+                        Feedback App
+                    </Link>
                     <ul className='right'>
-                        <li><a href='auth/google'>Login With Google</a></li>
+                        {this.renderContent()}
                     </ul>
                 </div>
             </nav>
@@ -15,4 +34,8 @@ class Header extends Component {
     }
 }
 
-export default Header;
+function mapStateToProps({auth}) {
+    return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
