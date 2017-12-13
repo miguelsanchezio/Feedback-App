1ba786351800eec04e1c595fa3703afe2963eef1
Miguel Sanchez <msanchez.anaheim@gmail.com>
2017-12-12 23:01:42 -0800

    Finished working on the backend logic of receiving user votes, the database is now properly updating whenever a user submits a vote

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -11,14 +11,14 @@ const Survey = mongoose.model('surveys');
 
 module.exports = (app) => {
 
-    app.get('/api/surveys/thanks', (req, res) => {
+    app.get('/api/surveys/:surveyId/:choice', (req, res) => {
         res.send('Thanks for voting.');
     })
     
     app.post('/api/surveys/webhooks', (req, res) => {
         const p = new Path('/api/surveys/:surveyId/:choice');
 
-        const events = _.chain(req.body)
+        _.chain(req.body)
             .map(({email, url}) => {
                 const match = p.test(new URL(url).pathname);
                 if(match) {
@@ -27,9 +27,19 @@ module.exports = (app) => {
             })
             .compact()
             .uniqBy('email', 'surveyId')
+            .each(({ surveyId, email, choice }) => {
+                Survey.updateOne({
+                    _id: surveyId,
+                    recipients: {
+                        $elemMatch: { email: email, responded: false }
+                    }
+                }, {
+                    $inc: { [choice]: 1 },
+                    $set: { 'recipients.$.responded': true },
+                    lastResponded: new Date()
+                }).exec();
+            })
             .value();
-
-        console.log(events);
 
         res.send({});
     });
